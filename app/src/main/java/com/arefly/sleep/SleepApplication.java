3007dffee539f6b3e433ffcc8ec2bdaa8f0ebb17
Jason Ho <eflyjason@gmail.com>
2016-08-03 15:08:56 +0800

    add class SleepApplication (extends Application)

---
+++ app/src/main/java/com/arefly/sleep/SleepApplication.java
@@ -0,0 +1,25 @@
+package com.arefly.sleep;
+
+import android.app.Application;
+
+import com.orhanobut.logger.LogLevel;
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by eflyjason on 3/8/2016.
+ */
+public class SleepApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Logger.init()
+                .methodCount(0)
+                .hideThreadInfo()
+                .logLevel(BuildConfig.DEBUG ? LogLevel.FULL : LogLevel.NONE);       // default LogLevel.FULL
+
+        //Logger.v("BuildConfig.DEBUG = " + BuildConfig.DEBUG);
+        Logger.i("SleepApplication onCreate()");
+    }
+}
