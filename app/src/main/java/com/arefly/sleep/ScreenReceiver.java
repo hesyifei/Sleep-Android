36645ea99ba8d97199f8c40650860fd14b3b7873
Jason Ho <eflyjason@gmail.com>
2016-08-03 15:27:45 +0800

    add class ScreenService & class ScreenReceiver

---
+++ app/src/main/java/com/arefly/sleep/ScreenReceiver.java
@@ -0,0 +1,51 @@
+package com.arefly.sleep;
+
+import android.app.KeyguardManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by eflyjason on 3/8/2016.
+ */
+public class ScreenReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Logger.i("ScreenReceiver onReceive()");
+
+        Logger.v("intent action: " + intent.getAction());
+
+        // http://baroqueworksdev.blogspot.hk/2012/09/how-to-handle-screen-onoff-and-keygurad.html
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_SHUTDOWN) || action.equals(context.getResources().getString(R.string.htc_action_quickboot_poweroff))) {
+            Logger.v("ACTION_SHUTDOWN || QUICKBOOT_POWEROFF");
+            saveLockData();
+        } else if (action.equals(Intent.ACTION_BOOT_COMPLETED) || action.equals(context.getResources().getString(R.string.htc_action_quickboot_poweron))) {
+            Logger.v("ACTION_BOOT_COMPLETED || QUICKBOOT_POWERON");
+        }
+
+        if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+            Logger.v("ACTION_SCREEN_OFF");
+        } else if (action.equals(Intent.ACTION_SCREEN_ON)) {
+            KeyguardManager mKeyguard = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+            if (mKeyguard.inKeyguardRestrictedInputMode()) {
+                Logger.v("ACTION_SCREEN_ON: locked");
+            } else {
+                Logger.v("ACTION_SCREEN_ON: unlocked");
+                saveLockData();
+            }
+        } else if (action.equals(Intent.ACTION_USER_PRESENT)) {
+            Logger.v("ACTION_USER_PRESENT");
+            saveLockData();
+        }
+
+    }
+
+    private void saveLockData() {
+        Logger.d("saveLockStatus() called");
+    }
+
+}
---
+++ app/src/main/java/com/arefly/sleep/ScreenService.java
@@ -0,0 +1,81 @@
+package com.arefly.sleep;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by eflyjason on 3/8/2016.
+ */
+public class ScreenService extends Service {
+
+    BroadcastReceiver mReceiver;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.i("ScreenService onCreate()");
+
+        // TODO: Change StatisticActivity to YesterdayActivity
+        Intent notificationIntent = new Intent(this, StatisticActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("你昨天晚上睡了6個小時,今天睡久一點吧!\n\n點擊查看詳細統計記錄"))
+                .setContentTitle("晚上好!")
+                .setContentText("你昨天晚上睡了6個小時!")
+                .setContentIntent(notificationPendingIntent)
+                .build();
+
+        startForeground(88, notification);
+
+
+        IntentFilter screenStateFilter = new IntentFilter();
+        screenStateFilter.addAction(Intent.ACTION_USER_PRESENT);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        screenStateFilter.addAction(Intent.ACTION_SHUTDOWN);
+        screenStateFilter.addAction(getString(R.string.htc_action_quickboot_poweroff));
+        screenStateFilter.addAction(Intent.ACTION_BOOT_COMPLETED);
+        screenStateFilter.addAction(getString(R.string.htc_action_quickboot_poweron));
+        mReceiver = new ScreenReceiver();
+        registerReceiver(mReceiver, screenStateFilter);
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Logger.i("ScreenService onStartCommand()");
+
+        return START_STICKY;
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Logger.i("ScreenService onBind()");
+        return null;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Logger.i("ScreenService onDestroy()");
+
+        unregisterReceiver(mReceiver);
+    }
+
+}
