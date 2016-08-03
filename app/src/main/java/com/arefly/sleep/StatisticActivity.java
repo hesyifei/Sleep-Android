6df3667c54b1687bf5e03b469e7a3e33ce98ae71
Jason Ho <eflyjason@gmail.com>
2016-08-03 15:27:55 +0800

    general changes

--- app/src/main/java/com/arefly/sleep/StatisticActivity.java
+++ app/src/main/java/com/arefly/sleep/StatisticActivity.java
@@ -3,11 +3,17 @@ package com.arefly.sleep;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by eflyjason on 3/8/2016.
+ */
 public class StatisticActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Logger.i("StatisticActivity onCreate()");
         setContentView(R.layout.activity_statistic);
     }
 }
