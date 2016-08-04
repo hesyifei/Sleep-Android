dd2221697242496e512912bac3c80d6a3a7a55ee
Jason Ho <eflyjason@gmail.com>
2016-08-04 15:43:19 +0800

    only save saveLockData when screen is on after initServiceAndAlarm in BootReceiver

--- app/src/main/java/com/arefly/sleep/BootReceiver.java
+++ app/src/main/java/com/arefly/sleep/BootReceiver.java
@@ -3,6 +3,8 @@ package com.arefly.sleep;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.PowerManager;
 
 import com.orhanobut.logger.Logger;
 
@@ -15,8 +17,16 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(context.getResources().getString(R.string.htc_action_quickboot_poweron))) {
             Logger.v("ACTION_BOOT_COMPLETED || QUICKBOOT_POWERON");
+
             StatisticActivity.initServiceAndAlarm(context.getApplicationContext());
-            ScreenReceiver.saveLockData(true);
+
+            PowerManager powerManager = (PowerManager) context.getApplicationContext().getSystemService(Context.POWER_SERVICE);
+            boolean isScreenOn = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH&&powerManager.isInteractive() || Build.VERSION.SDK_INT<Build.VERSION_CODES.KITKAT_WATCH&&powerManager.isScreenOn();
+            Logger.e("isScreenOn: " + isScreenOn);
+
+            if (isScreenOn) {
+                ScreenReceiver.saveLockData(true);
+            }
         }
     }
 }
