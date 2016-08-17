2b1d764aad28cd5071d4e9df58cadcf8365d8e2a
Yifei He <eflyjason@gmail.com>
2016-08-17 12:44:03 +0800

    add MaterialCalendarView in fragment_record.xml

--- app/src/main/java/com/arefly/sleep/fragments/RecordFragment.java
+++ app/src/main/java/com/arefly/sleep/fragments/RecordFragment.java
@@ -1,6 +1,8 @@
 package com.arefly.sleep.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -10,11 +12,16 @@ import android.view.ViewGroup;
 import com.arefly.sleep.R;
 import com.arefly.sleep.activities.MainActivity;
 import com.orhanobut.logger.Logger;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 /**
  * Created by eflyjason on 15/8/2016.
  */
-public class RecordFragment extends Fragment {
+public class RecordFragment extends Fragment implements OnDateSelectedListener {
+
+    MaterialCalendarView widget;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,10 +40,21 @@ public class RecordFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Logger.i("RecordFragment onViewCreated()");
 
+        // TODO: start new acitivy to show sleep time
+        // TODO: add sleep time below the date button in calendar
+
         AppCompatActivity mainActivity = (AppCompatActivity) getActivity();
         MainActivity.setupDrawer(mainActivity, view);
         mainActivity.setTitle(getString(R.string.record_fragment_name));
 
+
+        widget = (MaterialCalendarView) view.findViewById(R.id.calendar_view);
+        widget.setOnDateChangedListener(this);
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @Nullable CalendarDay date, boolean selected) {
+        Logger.v("RecordFragment onDateSelected(" + date + ")");
     }
 
 }
