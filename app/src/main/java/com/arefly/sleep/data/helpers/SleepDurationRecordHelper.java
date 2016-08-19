d8dba630485d795bed9575739f44de7977e84e61
Yifei He <eflyjason@gmail.com>
2016-08-19 14:00:36 +0800

    show average sleep duration in StatisticsFragment optimize fragment_statistics layout

--- app/src/main/java/com/arefly/sleep/data/helpers/SleepDurationRecordHelper.java
+++ app/src/main/java/com/arefly/sleep/data/helpers/SleepDurationRecordHelper.java
@@ -1,7 +1,16 @@
 package com.arefly.sleep.data.helpers;
 
+import com.arefly.sleep.data.objects.SleepDurationRecord;
+import com.orhanobut.logger.Logger;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * Created by eflyjason on 12/8/2016.
@@ -11,4 +20,64 @@ public class SleepDurationRecordHelper {
 
     public static final String DATE_FORMAT = "yyyy/MM/dd";
     public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT, Locale.US);
+
+
+    /**
+     * remove all repeating date (e.g. 2015/06/07+2015/06/07+2015/06/08 -> 2015/06/07+2015/06/08)
+     *
+     * @param realm Realm
+     * @return RealmResults which is lazy (i.e. can be modified directly)
+     */
+    public static RealmResults<SleepDurationRecord> removeAllRepeatingDate(Realm realm) {
+        RealmResults<SleepDurationRecord> allSleepDurationRecord = realm.where(SleepDurationRecord.class)
+                .findAllSorted("startTime", Sort.ASCENDING);
+        Logger.v("allSleepDurationRecord: " + allSleepDurationRecord);
+
+
+        List<Integer> locationNeededToBeRemoved = new ArrayList<>();
+        for (int i = 0; i < allSleepDurationRecord.size(); i++) {
+            SleepDurationRecord eachRecord = allSleepDurationRecord.get(i);
+            Logger.v("allSleepDurationRecord[" + i + "]: " + eachRecord);
+            if (i + 1 <= allSleepDurationRecord.size() - 1) {
+                // If have next record
+                SleepDurationRecord nextRecord = allSleepDurationRecord.get(i + 1);
+                String thisRecordDate = eachRecord.getDate();
+                String nextRecordDate = nextRecord.getDate();
+                if (thisRecordDate.equals(nextRecordDate)) {
+                    locationNeededToBeRemoved.add(i);
+                }
+            }
+        }
+        Logger.v("locationNeededToBeRemoved: " + locationNeededToBeRemoved);
+        if (!locationNeededToBeRemoved.isEmpty()) {
+            realm.beginTransaction();
+            for (Integer locationInteger : locationNeededToBeRemoved) {
+                Logger.v("prepare to remove " + locationInteger + " (" + allSleepDurationRecord.get(locationInteger) + ")");
+                // Can simply delete as realm object is lazy
+                allSleepDurationRecord.deleteFromRealm(locationInteger);
+            }
+            realm.commitTransaction();
+            Logger.v("locationNeededToBeRemoved removed from realm. allSleepDurationRecord: " + allSleepDurationRecord);
+        }
+
+        return allSleepDurationRecord;
+    }
+
+
+    public static long getAverageSleepDuration(RealmResults<SleepDurationRecord> realmResults) {
+        int daysCount = realmResults.size();
+
+        if (daysCount <= 0) {
+            return -1;
+        }
+
+        long returnDuration = 0;
+        for (int i = 0; i < realmResults.size(); i++) {
+            SleepDurationRecord eachRecord = realmResults.get(i);
+            Logger.v("realmResults[" + i + "]: " + eachRecord);
+            returnDuration += eachRecord.getDuration();
+        }
+        return returnDuration/daysCount;
+    }
+
 }
--- app/src/main/java/com/arefly/sleep/fragments/StatisticsFragment.java
+++ app/src/main/java/com/arefly/sleep/fragments/StatisticsFragment.java
@@ -5,9 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.arefly.sleep.R;
+import com.arefly.sleep.data.helpers.SleepDurationRecordHelper;
+import com.arefly.sleep.data.objects.SleepDurationRecord;
+import com.arefly.sleep.helpers.GlobalFunction;
 import com.orhanobut.logger.Logger;
+
+import java.util.Calendar;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * Created by eflyjason on 10/8/2016.
@@ -32,7 +42,45 @@ public class StatisticsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Logger.i("DayInfoFragment onCreateView()");
 
+        TextView upperLabel = (TextView) view.findViewById(R.id.statistics_upper_label);
+        TextView upperLabelSmall = (TextView) view.findViewById(R.id.statistics_upper_label_small);
+
+
         // TODO: startDate after -7 day 00:00 (e.g.)
+        Realm realm = Realm.getDefaultInstance();
+
+        SleepDurationRecordHelper.removeAllRepeatingDate(realm);
+
+
+        Calendar toBeCheckedStartTimeCal = Calendar.getInstance();
+        toBeCheckedStartTimeCal.set(Calendar.MILLISECOND, 0);
+        toBeCheckedStartTimeCal.set(Calendar.SECOND, 0);
+        toBeCheckedStartTimeCal.set(Calendar.MINUTE, 0);
+        toBeCheckedStartTimeCal.set(Calendar.HOUR_OF_DAY, 0);
+        toBeCheckedStartTimeCal.add(Calendar.DATE, -7);
+
+
+        RealmResults<SleepDurationRecord> allSleepDurationRecordInDays = realm.where(SleepDurationRecord.class)
+                .greaterThanOrEqualTo("startTime", toBeCheckedStartTimeCal.getTime())
+                .findAllSorted("startTime", Sort.ASCENDING);
+
+        Logger.v("allSleepDurationRecordInDays: " + allSleepDurationRecordInDays);
+
+
+        long averageSleepDuration = SleepDurationRecordHelper.getAverageSleepDuration(allSleepDurationRecordInDays);
+        Logger.v("averageSleepDuration: " + averageSleepDuration);
+
+        String averageSleepDurationString;
+        if (averageSleepDuration == -1) {
+            averageSleepDurationString = "N/A";
+        } else {
+            averageSleepDurationString = GlobalFunction.getHoursAndMinutesString(averageSleepDuration, getContext());
+        }
+
+
+        upperLabel.setText(averageSleepDurationString);
+        upperLabelSmall.setText("Average Sleep Duration");
+
     }
 
 }
