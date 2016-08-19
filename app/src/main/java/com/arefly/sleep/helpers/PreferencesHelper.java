3c302345f8c735cad41de6f7306eb69c63271d27
Yifei He <eflyjason@gmail.com>
2016-08-19 10:53:18 +0800

    use constant in PreferencesHelper for id instead of in strings.xml

--- app/src/main/java/com/arefly/sleep/helpers/PreferencesHelper.java
+++ app/src/main/java/com/arefly/sleep/helpers/PreferencesHelper.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import com.arefly.sleep.R;
-
 /**
  * Created by eflyjason on 5/8/2016.
  */
 public class PreferencesHelper {
+
+    public static final String BOOL_IS_WAKEN_UP = "isWakenUp";
+    public static final String STRING_EARLIEST_SLEEP_TIME = "earliestSleepTime";
+    public static final String STRING_EARLIEST_WAKE_TIME = "earliestWakeTime";
+    public static final String LONG_LONGEST_IGNORE_SECONDS = "longestIgnoreSeconds";
+
 
     public static SharedPreferences getPreferences(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
@@ -17,43 +21,43 @@ public class PreferencesHelper {
 
 
     public static boolean getIsWakenUpBool(Context context) {
-        return getPreferences(context).getBoolean(context.getResources().getString(R.string.bool_is_waken_up), false);
+        return getPreferences(context).getBoolean(BOOL_IS_WAKEN_UP, false);
     }
 
     public static String getSleepTimeString(Context context) {
-        return getPreferences(context).getString(context.getResources().getString(R.string.string_earliest_sleep_time), "20:00");
+        return getPreferences(context).getString(STRING_EARLIEST_SLEEP_TIME, "20:00");
     }
 
     public static String getWakeTimeString(Context context) {
-        return getPreferences(context).getString(context.getResources().getString(R.string.string_earliest_wake_time), "06:00");
+        return getPreferences(context).getString(STRING_EARLIEST_WAKE_TIME, "06:00");
     }
 
     public static long getLongestIgnoreSeconds(Context context) {
-        return getPreferences(context).getLong(context.getResources().getString(R.string.long_longest_ignore_seconds), 30);
+        return getPreferences(context).getLong(LONG_LONGEST_IGNORE_SECONDS, 30);
     }
 
 
     public static void setIsWakenUpBool(boolean isWakenUp, Context context) {
         SharedPreferences.Editor editor = getPreferences(context).edit();
-        editor.putBoolean(context.getResources().getString(R.string.bool_is_waken_up), isWakenUp);
+        editor.putBoolean(BOOL_IS_WAKEN_UP, isWakenUp);
         editor.apply();
     }
 
     public static void setSleepTimeString(String time, Context context) {
         SharedPreferences.Editor editor = getPreferences(context).edit();
-        editor.putString(context.getResources().getString(R.string.string_earliest_sleep_time), time);
+        editor.putString(STRING_EARLIEST_SLEEP_TIME, time);
         editor.apply();
     }
 
     public static void setWakeTimeString(String time, Context context) {
         SharedPreferences.Editor editor = getPreferences(context).edit();
-        editor.putString(context.getResources().getString(R.string.string_earliest_wake_time), time);
+        editor.putString(STRING_EARLIEST_WAKE_TIME, time);
         editor.apply();
     }
 
     public static void setLongestIgnoreSeconds(long seconds, Context context) {
         SharedPreferences.Editor editor = getPreferences(context).edit();
-        editor.putLong(context.getResources().getString(R.string.long_longest_ignore_seconds), seconds);
+        editor.putLong(LONG_LONGEST_IGNORE_SECONDS, seconds);
         editor.apply();
     }
 
